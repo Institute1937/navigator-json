b4250d9ddb08ddc614bcbe4600bd96073133aad5
cerico <garethrobertlee@gmail.com>
2017-11-17 07:13:08 +0000

    use fetch for json

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
-import emails from './email-data.json'
+// import emails from './email-data.json'
 
 export class Index {
 
     constructor(){
-      console.log(emails.length);
+      // console.log(emails.length);
+      const url = './src/js/email-data.json'
+      fetch(url).then(r => r.json())
+      .then(emails => console.log(emails.length))
     }
   };
   document.addEventListener('DOMContentLoaded', () => {
